c103e88d0890b0c3ea905d2780270f3a3f5dd78d
Noah <1134312090@qq.com>
2019-07-17 21:48:30 +0800

    new Select_sort

---
+++ select_sort/select_sort/select_sort.cpp
@@ -0,0 +1,37 @@
+﻿// select_sort.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+void select_sort(vector<int> &arr)
+{
+	int j=0;
+	for (int i = 0; i < arr.size(); i++) {
+		for ( j = i + 1; j < arr.size(); j++) {//将最小的元素移到最后位置
+			if (arr[j] > arr[j - 1])
+			{
+				swap(arr[j - 1], arr[j]);
+			}
+	 }
+		swap(arr[i], arr[j-1]);//循环最后一次执行了j++,超出数组的范围
+	}
+}
+
+
+int main()
+{
+
+	vector<int> arr = { 2,3,6,6,7,1,1,8,4,5,9 };
+	select_sort(arr);
+
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] << ends;
+	}
+}
